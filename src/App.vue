01148c91794799a525ae8b5c5c241f5af62d31c5
ToboBrick <di.gomes95@gmail.com>
2022-01-08 23:38:52 +0000

    Created MiniCard Component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
-    <div class="container bg-gray-100 px-32 py-32">
-        <HeroSection title="Hello World!"></HeroSection>
+  <div class="container bg-gray-100 px-32 py-32">
+    <HeroSection title="Hello World!"></HeroSection>
+    <div class="grid grid-cols-2 gap-9">
+      <MiniCard v-for="(item, i) in 6" :key="i" />
     </div>
+  </div>
 </template>
 
 <script>
 import HeroSection from "./components/HeroSection.vue";
+import MiniCard from "./components/MiniCard.vue";
+
 export default {
-    name: "App",
-    components: {
-        HeroSection,
-    },
+  name: "App",
+  components: {
+    HeroSection,
+    MiniCard,
+  },
 };
 </script>
---
+++ src/components/MiniCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="mini-card bg-white rounded-lg p-8 flex max-w-lg items-center">
+    <div
+      class="num rounded-full mr-5 w-20 h-20 bg-opacity-70 bg-blue-100 flex justify-center items-center flex-none"
+    >
+      <span class="text-blue-500 bg-opacity-80 text-2xl font-bold">01</span>
+    </div>
+    <div class="details flex-auto">
+      <h2 class="text-lg tracking-wide font-bold">Collect Ideas</h2>
+      <p class="text-gray-600">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </p>
+    </div>
+  </div>
+</template>
