28ab3eaeb36600aa7dc36872725d45bb36f36f6d
Nuno Fernandes <nmspf1@gmail.com>
2022-01-08 19:42:19 +0000

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container bg-gray-100 px-32 py-32">
+        <HeroSection title="Hello World!"></HeroSection>
+    </div>
+</template>
+
+<script>
+import HeroSection from "./components/HeroSection.vue";
+export default {
+    name: "App",
+    components: {
+        HeroSection,
+    },
+};
+</script>
---
+++ src/components/HeroSection.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p>{{ title }}</p>
+
+        <ul>
+            <li>Tailwind Flexbox</li>
+            <li>Html best practices - section, articles, headings(h1,h2) hierarchy</li>
+            <li>Tailwind responsiveness (https://tailwindcss.com/docs/responsive-design)</li>
+            <li>Vue <code>v-for</code> loops</li>
+            <li>Vue Components</li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    props: ["title"],
+};
+</script>
