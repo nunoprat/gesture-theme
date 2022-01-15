201c63932ebbe62539589f6e0d0e3faf3696b020
ToboBrick <di.gomes95@gmail.com>
2022-01-15 22:57:40 +0000

    Resume section created and finished

--- src/components/sections/ResumeSection.vue
+++ src/components/sections/ResumeSection.vue
@@ -1,20 +1,22 @@
 <template>
-    <section class="section">
-        <div class="container flex items-center justify-between">
-            <div class="w-5/12">
-                <h6 class="up-heading">About Us</h6>
-                <h2 class="heading-1 w-full">The Best Solutions For You</h2>
-            </div>
-            <div class="w-1/12">
-                <div
-                    class="bar mx-auto h-0.5 w-10 bg-white bg-opacity-50 transform rotate-45"
-                ></div>
-            </div>
-            <p class="ml-6 w-6/12">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid possimus vero
-                tempore eius, temporibus id natus similique quam adipisci earum esse exercitationem
-                aperiam, tempora delectus accusantium praesentium quae doloribus sed!
-            </p>
-        </div>
-    </section>
+  <section class="section">
+    <div
+      class="container flex flex-col lg:flex-row items-center justify-between"
+    >
+      <div class="w-full lg:w-5/12">
+        <h6 class="up-heading">About Us</h6>
+        <h2 class="heading-1">The Best Solutions For You</h2>
+      </div>
+      <div class="w-full py-10 lg:py-0 lg:w-1/12">
+        <div
+          class="bar mx-auto h-0.5 w-10 bg-white bg-opacity-50 transform rotate-45"
+        ></div>
+      </div>
+      <p class="ml-6 w-full lg:w-6/12 font-thin leading-loose text-xl">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid
+        possimus vero tempore eius, temporibus id natus similique quam adipisci
+        earum esse exercitationem aperiam.
+      </p>
+    </div>
+  </section>
 </template>
