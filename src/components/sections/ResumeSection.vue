3a75415e2559e6d22b3215639f943c9a119a6997
nunoprat <nmspf1@gmail.com>
2022-01-15 20:14:48 +0000

    panucas

--- src/components/sections/ResumeSection.vue
+++ src/components/sections/ResumeSection.vue
@@ -1,19 +1,20 @@
 <template>
-  <section class="section">
-    <div class="container flex items-center justify-between">
-      <div class="">
-        <h6 class="up-heading">About Us</h6>
-        <h2 class="heading-1 w-full">The Best Solutions For You</h2>
-      </div>
-      <div
-        class="bar h-0.5 w-24 bg-white bg-opacity-50 transform rotate-45"
-      ></div>
-      <p class="ml-6">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid
-        possimus vero tempore eius, temporibus id natus similique quam adipisci
-        earum esse exercitationem aperiam, tempora delectus accusantium
-        praesentium quae doloribus sed!
-      </p>
-    </div>
-  </section>
+    <section class="section">
+        <div class="container flex items-center justify-between">
+            <div class="w-5/12">
+                <h6 class="up-heading">About Us</h6>
+                <h2 class="heading-1 w-full">The Best Solutions For You</h2>
+            </div>
+            <div class="w-1/12">
+                <div
+                    class="bar mx-auto h-0.5 w-10 bg-white bg-opacity-50 transform rotate-45"
+                ></div>
+            </div>
+            <p class="ml-6 w-6/12">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid possimus vero
+                tempore eius, temporibus id natus similique quam adipisci earum esse exercitationem
+                aperiam, tempora delectus accusantium praesentium quae doloribus sed!
+            </p>
+        </div>
+    </section>
 </template>
