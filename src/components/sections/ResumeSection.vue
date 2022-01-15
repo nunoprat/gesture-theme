1d4dfa181315d566cc14a3196bd6008fec207fc0
ToboBrick <di.gomes95@gmail.com>
2022-01-15 20:04:30 +0000

    Update ResumeSection.vue

--- src/components/sections/ResumeSection.vue
+++ src/components/sections/ResumeSection.vue
@@ -1,15 +1,19 @@
 <template>
   <section class="section">
-    <div class="container">
-      <h6 class="up-heading">About Us</h6>
-      <h2 class="heading-1">Lorem ipsum dolor sit, amet consectetur</h2>
-      <p>
+    <div class="container flex items-center justify-between">
+      <div class="">
+        <h6 class="up-heading">About Us</h6>
+        <h2 class="heading-1 w-full">The Best Solutions For You</h2>
+      </div>
+      <div
+        class="bar h-0.5 w-24 bg-white bg-opacity-50 transform rotate-45"
+      ></div>
+      <p class="ml-6">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid
         possimus vero tempore eius, temporibus id natus similique quam adipisci
         earum esse exercitationem aperiam, tempora delectus accusantium
         praesentium quae doloribus sed!
       </p>
-      <p>pralápié</p>
     </div>
   </section>
 </template>
