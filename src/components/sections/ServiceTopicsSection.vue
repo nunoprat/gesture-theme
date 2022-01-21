b80f81664fde6fafd8c992680d78a676163699b1
ToboBrick <di.gomes95@gmail.com>
2022-01-21 18:45:17 +0000

    Help

---
+++ src/components/sections/ServiceTopicsSection.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="section">
+    <div class="container py-10">
+      <h6 class="up-heading">Services</h6>
+      <h2 class="heading-1 pb-10">Our Simple AF Process</h2>
+      <div class="flex flex-col lg:flex-row">
+        <div class="bg-secondary bg-opacity-30 px-6 py-10 mb-4 rounded-2xl">
+          <img src="@/assets/Vector.svg" alt="Design" />
+          <p class="paragraph-base">
+            Seamlessly architect e-business experiences without vertical
+            strategic theme areas. Credibly formulate
+          </p>
+        </div>
+        <div class="bg-secondary bg-opacity-30 px-6 py-10 mb-4 rounded-2xl">
+          <img src="@/assets/Vector.svg" alt="Development" />
+          <p class="paragraph-base">
+            Seamlessly architect e-business experiences without vertical
+            strategic theme areas. Credibly formulate
+          </p>
+        </div>
+        <div class="bg-secondary bg-opacity-30 px-6 py-10 mb-4 rounded-2xl">
+          <img src="@/assets/Vector.svg" alt="Launch and Deploy" />
+          <p class="paragraph-base">
+            Seamlessly architect e-business experiences without vertical
+            strategic theme areas. Credibly formulate
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <ResumeSection />
   <ResumeImageSection />
+  <ServiceTopicsSection />
 </template>
 
 <script>
 import ResumeSection from "@/components/sections/ResumeSection.vue";
 import ResumeImageSection from "@/components/sections/ResumeImageSection.vue";
+import ServiceTopicsSection from "@/components/sections/ServiceTopicsSection.vue";
 export default {
   name: "Home",
   components: {
     ResumeSection,
     ResumeImageSection,
+    ServiceTopicsSection,
   },
 };
 </script>
